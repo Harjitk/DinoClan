6f4bca16aad1986967ee4eb572fa2e5ac4d141ae
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-07-14 10:58:45 +0100

    Seeded more last names

--- src/main/java/models/humans/Human.java
+++ src/main/java/models/humans/Human.java
@@ -24,9 +24,11 @@ public abstract class Human implements Iedible, Imeaty {
 
         firstNames.add("Harjit"); firstNames.add("Richard"); firstNames.add("John"); firstNames.add("Steve"); firstNames.add("Louise"); firstNames.add("Colin"); firstNames.add("Cleyra"); firstNames.add("Stephan"); firstNames.add("Adri"); firstNames.add("Angelina"); firstNames.add("Campbell"); firstNames.add("Craig"); firstNames.add("Debi"); firstNames.add("Derek"); firstNames.add("Farheen"); firstNames.add("Greg"); firstNames.add("Jack"); firstNames.add("Joe"); firstNames.add("Matt"); firstNames.add("Pete"); firstNames.add("Roddy"); firstNames.add("Steven"); firstNames.add("Tara");
 
-        lastNames.add("Singh"); lastNames.add("Phillips-Kerr"); lastNames.add("McCollum"); lastNames.add("Meiklejohn"); lastNames.add("Reid"); lastNames.add("Bell"); lastNames.add("Blyth"); lastNames.add("Miller"); lastNames.add("McDowell"); lastNames.add("Skea"); lastNames.add("Leach"); lastNames.add("Murning"); lastNames.add("Bonner");
+        lastNames.add("Singh"); lastNames.add("Phillips-Kerr"); lastNames.add("McCollum"); lastNames.add("Meiklejohn"); lastNames.add("Reid"); lastNames.add("Bell"); lastNames.add("Blyth"); lastNames.add("Miller"); lastNames.add("McDowell"); lastNames.add("Skea"); lastNames.add("Leach"); lastNames.add("Murning"); lastNames.add("Bonner"); lastNames.add("Pollock");
 
     }
+
+//    Ask John whether there's a better place to store these
 
 
     public String randName(){
