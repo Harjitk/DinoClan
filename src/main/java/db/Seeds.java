582ae979a87c8502607f477b65d103465e92912c
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-07-16 16:19:32 +0100

    Updated seeds with new paddocks and generated food

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -27,18 +27,22 @@ public class Seeds {
         DBPark.addPaddock(park, stegzDwelling);
         Paddock velociraptorDen = new Paddock("Velociraptor den", 5, park);
         DBPark.addPaddock(park, velociraptorDen);
+        Paddock diploJungle = new Paddock("Diplojungle", 10, park);
+        DBPark.addPaddock(park, diploJungle);
+        Paddock trexDuplex = new Paddock("Trex Duplex", 1, park);
+        DBPark.addPaddock(park, diploJungle);
 
-        Velociraptor velociraptor = new Velociraptor("Tyrant", 15, 500000, 5, park, holdingPen);
+        Velociraptor velociraptor = new Velociraptor("Tyrant", 15, 500000, 5, park, velociraptorDen);
         DBPark.buyDinosaur(velociraptor, holdingPen);
-        Velociraptor velociraptor2 = new Velociraptor("Trouble", 15, 500000, 5, park, holdingPen);
+        Velociraptor velociraptor2 = new Velociraptor("Trouble", 15, 500000, 5, park, velociraptorDen);
         DBPark.buyDinosaur(velociraptor2, holdingPen);
-        Velociraptor velociraptor3 = new Velociraptor("Terminator", 15, 500000, 5, park, holdingPen);
+        Velociraptor velociraptor3 = new Velociraptor("Terminator", 15, 500000, 5, park, velociraptorDen);
         DBPark.buyDinosaur(velociraptor3, holdingPen);
-        Diplodocus diplodocus = new Diplodocus("Todd", 35, 700000, 5, park, holdingPen);
+        Diplodocus diplodocus = new Diplodocus("Todd", 35, 700000, 5, park, diploJungle);
         DBPark.buyDinosaur(diplodocus, holdingPen);
-        Stegosaurus stegosaurus = new Stegosaurus("Stegz", 45, 850000, 6, park, holdingPen);
+        Stegosaurus stegosaurus = new Stegosaurus("Stegz", 45, 850000, 6, park, stegzDwelling);
         DBPark.buyDinosaur(stegosaurus, holdingPen);
-        Tyrannosaurus tyrannosaurus = new Tyrannosaurus("Rex", 55, 950000, 7, park, holdingPen);
+        Tyrannosaurus tyrannosaurus = new Tyrannosaurus("Rex", 55, 950000, 7, park, trexDuplex);
         DBPark.buyDinosaur(tyrannosaurus, holdingPen);
 
         Visitor richard = new Visitor("Richard", 1000, park);
@@ -49,10 +53,7 @@ public class Seeds {
         ParkStaff debzStaff = new ParkStaff("debz", 1000, park);
         DBPark.addParkStaff(park, debzStaff);
 
-        Meat meat = new Meat();
-        DBHelper.saveOrUpdate(meat);
-        Plant plant = new Plant();
-        DBHelper.saveOrUpdate(plant);
+        DBPark.generateFood(park, 200);
 
 
         List<Visitor> foundVisitors = DBHelper.getAll(Visitor.class);
